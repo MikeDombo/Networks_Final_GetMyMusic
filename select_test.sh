69ca9cb5d54d66db7fd0e10c005d70afa00cc94d
Michael Dombrowski <mikepi3.14@gmail.com>
2018-04-22 16:13:11 -0400

    Running, but not exactly working :frowning:

--- select_test.sh
+++ select_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {0..1024..1}
-  do 
-     make -f client || exit
- done
+for i in `seq 1 10`
+  do
+    ./Project4Client -p 30600 -s 127.0.0.1 -d ./testClientDir > /dev/null 2>&1 &
+  done
